661cc261d1ca0b6be8fa1c00d8f2449cdf43d17b
FGHovitech <f.giezek@hovitech.de>
2025-02-16 19:46:22 +0100

    Add files via upload

--- js/eingabemaske.js
+++ js/eingabemaske.js
@@ -1,84 +1,190 @@
 $(document).ready(function() {
-    // Nebenzeiten-Plus-Button: Fügt innerhalb eines inneren Abschnitts eine weitere Nebenzeitenszeile hinzu.
-    $(document).on('click', '.add-nebenzeiten', function() {
-      var $group = $(this).closest('.nebenzeiten-group');
-      var $newRow = $group.find('.nebenzeiten-row').first().clone();
-      $newRow.find('input').val('');
-      $newRow.find('select').prop('selectedIndex', 0);
-  
-      $(this).parent().before($newRow);
-  
-      // Entferne das Attribut, damit das neue Dropdown neu initialisiert wird
-      $newRow.find('.dropdown-menu').removeAttr('data-dropdown-inited');
-  
-      // Dropdowns im neuen Nebenzeiten-Block initialisieren
-      initDropdowns($newRow.get(0));
+  var originalInnerSectionTemplate = null;
+    // Initial beim Laden der Seite: Minus‑Buttons entsprechend der vorhandenen Zeilen ausblenden oder einblenden
+  $(document).ready(function() {
+    $('.gewichtsklasse-group').each(function() {
+      updateMinusButtonForGewichtsklasse($(this));
     });
-  
-    // Neuer Abschnitt innerhalb eines Mitarbeiters:
-    $(document).on('click', '.add-inner-section', function() {
-      var $innerContainer = $(this).closest('.inner-container');
-      var $wrapper = $innerContainer.find('.inner-section-wrapper');
-      var $btn = $(this).detach();
-  
-      // Klonen des letzten vorhandenen Abschnitts
-      var $newSection = $wrapper.children('.eingabemaske-abschnitt').last().clone();
-      $newSection.find('input').val('');
-      $newSection.find('select').prop('selectedIndex', 0);
-  
-      // Neuen Abschnitt anhängen und den Button unter diesen einfügen
-      $wrapper.append($newSection);
-      $newSection.after($btn);
-  
-      // Entferne das Attribut für alle Dropdowns im neuen Abschnitt
-      $newSection.find('.dropdown-menu').removeAttr('data-dropdown-inited');
-  
-      // Dropdowns im neuen Abschnitt initialisieren
-      initDropdowns($newSection.get(0));
+    $('.eingabemaske-row').each(function() {
+      updateMinusButtonForNebenzeiten($(this));
     });
-  
-    // Neuer Mitarbeiter:
-    $(document).on('click', '.add-eingabemaske', function() {
-      var $employeeBtn = $(this).detach();
-      var $newRow = $('#eingabemaske-container .eingabemaske-row').first().clone();
-      $newRow.find('input').val('');
-      $newRow.find('select').prop('selectedIndex', 0);
-  
-      // In jedem inner-container: Nur den ersten inneren Abschnitt beibehalten
-      $newRow.find('.inner-container').each(function() {
-        var $wrapper = $(this).find('.inner-section-wrapper');
-        $wrapper.children('.eingabemaske-abschnitt').not(':first').remove();
-      });
-  
-      $('#eingabemaske-container').append($newRow);
-      $newRow.after($employeeBtn);
-  
-      // Entferne das Attribut für alle Dropdowns im neuen Mitarbeiter-Block
-      $newRow.find('.dropdown-menu').removeAttr('data-dropdown-inited');
-  
-      // Dropdowns im neuen Mitarbeiter-Block initialisieren
-      initDropdowns($newRow.get(0));
-    });
+    originalInnerSectionTemplate = $('.inner-section-wrapper .eingabemaske-abschnitt').first().clone();
   });
 
-  // Plus-Knopf für Gewichtsklasse, Stückzahl & VGZ:
-$(document).on('click', '.add-gewichtsklasse', function() {
-  var $group = $(this).closest('.gewichtsklasse-group');
-  // Klonen der ersten Zeile der Gruppe
-  var $newRow = $group.find('.gewichtsklasse-row').first().clone();
-  // Alle Eingabefelder leeren
-  $newRow.find('input').val('');
-  // Entferne Attribut für Dropdown-Neuinitialisierung
-  $newRow.find('.dropdown-menu').removeAttr('data-dropdown-inited');
-  
-  // Füge die neue Zeile oberhalb des Plus-Buttons ein
-  $(this).parent().before($newRow);
-  
-  // Initialisiere die Dropdowns im neuen Block neu
-  initDropdowns($newRow.get(0));
-});
 
+  // Nebenzeiten-Plus-Button: Fügt innerhalb eines inneren Abschnitts eine weitere Nebenzeitenszeile hinzu.
+  $(document).on('click', '.add-nebenzeiten', function() {
+    var $group = $(this).closest('.nebenzeiten-group');
+    var $template = $group.find('.nebenzeiten-row').first();
+    var $newRow;
   
+    if ($template.length === 0) {
+      // Falls keine Nebenzeiten-Zeile vorhanden ist, erzeuge eine neue Zeile aus einem Template-HTML
+      var templateHtml = '' +
+        '<div class="d-flex input-group mb-1 nebenzeiten-row">' +
+          '<div class="nebenzeitenDropdown dropdown-menu d-block position-relative pt-0 mx-0 rounded-3 shadow" data-bs-theme="light">' +
+            '<form>' +
+              '<input type="search" class="form-control" autocomplete="off" placeholder="Nebenzeit">' +
+            '</form>' +
+            '<ul class="list-unstyled mb-0" style="position: absolute; top: 100%; left: 0; width: 100%; display: none;">' +
+              '<li>' +
+                '<a class="dropdown-item d-flex align-items-center gap-2 py-2" href="#">Vorgang 1</a>' +
+              '</li>' +
+              '<li>' +
+                '<a class="dropdown-item d-flex align-items-center gap-2 py-2" href="#">Vorgang 2</a>' +
+              '</li>' +
+            '</ul>' +
+          '</div>' +
+          '<input type="number" name="nebenzeiten_number[]" class="form-control minutenEingabe" placeholder="Minuten">' +
+        '</div>';
+      $newRow = $(templateHtml);
+    } else {
+      // Andernfalls klone die erste vorhandene Zeile
+      $newRow = $template.clone();
+    }
+    
+    // Leere die Eingaben
+    $newRow.find('input').val('');
+    $newRow.find('select').prop('selectedIndex', 0);
+  
+    // Füge die neue Zeile oberhalb des Buttons ein
+    $(this).parent().before($newRow);
+  
+    // Entferne das Attribut, damit das Dropdown neu initialisiert werden kann
+    $newRow.find('.dropdown-menu').removeAttr('data-dropdown-inited');
+  
+    // Initialisiere die Dropdowns im neuen Block
+    initDropdowns($newRow.get(0));
+  
+    // Aktualisiere die Sichtbarkeit der Nebenzeiten-Minus-Buttons im gesamten Mitarbeiter-Container
+    var $employeeContainer = $(this).closest('.eingabemaske-row');
+    updateMinusButtonForNebenzeiten($employeeContainer);
+  });
+  
+  // Neue Kostenstelle:
+  $(document).on('click', '.add-inner-section', function() {
+    var $innerContainer = $(this).closest('.inner-container');
+    var $wrapper = $innerContainer.find('.inner-section-wrapper');
+    var $btn = $(this).detach(); // Button vorübergehend entfernen
+  
+    // Erstelle einen neuen inneren Abschnitt aus dem Original-Template
+    var $newSection = originalInnerSectionTemplate.clone();
+  
+    // Füge den neuen Abschnitt in den Wrapper ein und hänge den Button wieder an
+    $wrapper.append($newSection);
+    $newSection.after($btn);
+  
+    // Entferne das Attribut für die Dropdown-Neuinitialisierung
+    $newSection.find('.dropdown-menu').removeAttr('data-dropdown-inited');
+  
+    // Initialisiere die Dropdowns im neuen Abschnitt
+    initDropdowns($newSection.get(0));
+  
+    // Aktualisiere die Sichtbarkeit der Nebenzeiten-Minus-Buttons:
+    var $employeeContainer = $(this).closest('.eingabemaske-row');
+    updateMinusButtonForNebenzeiten($employeeContainer);
+  });
+
+  // Neuer Mitarbeiter:
+  $(document).on('click', '.add-eingabemaske', function() {
+    var $employeeBtn = $(this).detach();
+    var $newRow = $('#eingabemaske-container .eingabemaske-row').first().clone();
+    $newRow.find('input').val('');
+    $newRow.find('select').prop('selectedIndex', 0);
+
+    // In jedem inner-container: Nur den ersten inneren Abschnitt beibehalten
+    $newRow.find('.inner-container').each(function() {
+      var $wrapper = $(this).find('.inner-section-wrapper');
+      $wrapper.children('.eingabemaske-abschnitt').not(':first').remove();
+    });
+
+    $('#eingabemaske-container').append($newRow);
+    $newRow.after($employeeBtn);
+
+    // Entferne das Attribut für alle Dropdowns im neuen Mitarbeiter-Block
+    $newRow.find('.dropdown-menu').removeAttr('data-dropdown-inited');
+
+    // Dropdowns im neuen Mitarbeiter-Block initialisieren
+    initDropdowns($newRow.get(0));
+  });
+  });
+
+  // Neue Gewichtsklasse, Stückzahl & VGZ:
+  $(document).on('click', '.add-gewichtsklasse', function() {
+    var $group = $(this).closest('.gewichtsklasse-group');
+    // Klonen der ersten Zeile der Gruppe
+    var $newRow = $group.find('.gewichtsklasse-row').first().clone();
+    // Eingabefelder leeren
+    $newRow.find('input').val('');
+    // Attribut für Dropdown-Neuinitialisierung entfernen
+    $newRow.find('.dropdown-menu').removeAttr('data-dropdown-inited');
+    
+    // Neue Zeile oberhalb des Plus-Buttons einfügen
+    $(this).parent().before($newRow);
+    
+    // Dropdowns im neuen Block initialisieren
+    initDropdowns($newRow.get(0));
+    
+    // Minus-Button Sichtbarkeit aktualisieren
+    updateMinusButtonForGewichtsklasse($group);
+  });
+
+
+  // Minus‑Button bei Gewichtsklasse
+  $(document).on('click', '.minus-gewichtsklasse', function() {
+    var $group = $(this).closest('.gewichtsklasse-group');
+    var $rows = $group.find('.gewichtsklasse-row');
+    if ($rows.length > 1) {
+      // Entfernt die letzte Gewichtsklasse-Zeile
+      $rows.last().remove();
+    }
+    updateMinusButtonForGewichtsklasse($group);
+  });
+
+  // Minus‑Button bei Nebenzeiten
+  $(document).on('click', '.minus-nebenzeiten', function() {
+    // Bestimme den gesamten Mitarbeiter-Container (eingabemaske‑row)
+    var $employeeContainer = $(this).closest('.eingabemaske-row');
+    var $allNebenzeiten = $employeeContainer.find('.nebenzeiten-row');
+    
+    // Falls weniger als zwei Nebenzeiten im gesamten Mitarbeiter vorhanden sind, passiert nichts.
+    if ($allNebenzeiten.length < 2) {
+      return;
+    }
+    
+    // Entferne in dem aktuellen Kostenstellen-Container (nebenzeiten-group) die letzte Nebenzeiten-Zeile
+    var $group = $(this).closest('.nebenzeiten-group');
+    var $rows = $group.find('.nebenzeiten-row');
+    if ($rows.length > 0) {
+      $rows.last().remove();
+    }
+    
+    updateMinusButtonForNebenzeiten($employeeContainer);
+  });
+  
+
+// Hilfsfunktion: Aktualisiert die Sichtbarkeit des Minus‑Buttons für Gewichtsklassen
+function updateMinusButtonForGewichtsklasse($group) {
+  var $rows = $group.find('.gewichtsklasse-row');
+  var $minusButton = $group.find('.minus-gewichtsklasse');
+  if ($rows.length > 1) {
+    $minusButton.show();
+  } else {
+    $minusButton.hide();
+  }
+}
+
+// Hilfsfunktion: Aktualisiert die Sichtbarkeit der Nebenzeiten‑Minus‑Buttons für einen Mitarbeiter-Container
+function updateMinusButtonForNebenzeiten($employeeContainer) {
+  // Zählt alle Nebenzeitenzeilen innerhalb des gesamten Mitarbeiters
+  var totalRows = $employeeContainer.find('.nebenzeiten-row').length;
+  if (totalRows > 1) {
+    $employeeContainer.find('.minus-nebenzeiten').show();
+  } else {
+    $employeeContainer.find('.minus-nebenzeiten').hide();
+  }
+}
+
   // DROPDOWN-MENÜ
   
   // Initialisiert ein einzelnes Dropdown-Element
@@ -159,4 +265,4 @@ $(document).on('click', '.add-gewichtsklasse', function() {
   document.addEventListener('DOMContentLoaded', function() {
     initDropdowns(document);
   });
-  
+ 
